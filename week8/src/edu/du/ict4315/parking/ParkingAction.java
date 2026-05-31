d9c2ff927f9df15c0a46871b98b6f9e85c02ee4e
Jake Collins <Jakehunter27@gmail.com>
2026-05-30 21:55:29 -0600

    Week8 done

--- week8/src/edu/du/ict4315/parking/ParkingAction.java
+++
@@ -1,5 +0,0 @@
-package edu.du.ict4315.parking;
-
-public interface ParkingAction {
-    void update(ParkingEvent event);
-}
--- week8/src/edu/du/ict4315/parking/ParkingEvent.java
+++
@@ -1,9 +0,0 @@
-package edu.du.ict4315.parking;
-
-import java.time.LocalDateTime;
-
-public record ParkingEvent(ParkingLot lot, ParkingPermit permit, LocalDateTime eventTime,
-                           edu.du.ict4315.parking.ParkingEvent.EventType eventType) {
-    public enum EventType {ENTER, EXIT}
-
-}
